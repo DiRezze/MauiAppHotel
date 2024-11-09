eb24aea58963514374dfbd87d4e75250775ad2f8
Gabriel B. Di Rezze <bortoletigabriel.drz@gmail.com>
2024-11-09 09:48:44 -0300

    Tratamento da seleção de data de check-in e check-out

--- Views/ContratacaoHospedagem.xaml.cs
+++ Views/ContratacaoHospedagem.xaml.cs
@@ -11,7 +11,17 @@ public partial class ContratacaoHospedagem : ContentPage
         PropriedadeApp = (App)Application.Current;
 
         pck_quarto.ItemsSource = PropriedadeApp.lista_quartos;
-	}
+
+        dtpck_checkin.MinimumDate = DateTime.Now;
+
+        dtpck_checkin.MaximumDate = DateTime.Now.AddMonths(1);
+
+
+        dtpck_checkout.MinimumDate = dtpck_checkin.Date.AddDays(1);
+
+        dtpck_checkout.MaximumDate = dtpck_checkin.Date.AddMonths(6);
+
+    }
 
     private void Button_Clicked(object sender, EventArgs e)
     {
@@ -35,6 +45,14 @@ public partial class ContratacaoHospedagem : ContentPage
         {
             DisplayAlert("Ops", ex.Message, "Ok");
         }
+    }
+
+    private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
+    {
+        DateTime dataSelecionada = e.NewDate;
+
+        dtpck_checkout.MinimumDate = dataSelecionada.AddDays(1);
+        dtpck_checkout.MaximumDate = dataSelecionada.AddMonths(6);
     }
 
 }
