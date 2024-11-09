6b10d27c3853fdd53689269a90bd1b3d72d67dc4
Gabriel B. Di Rezze <bortoletigabriel.drz@gmail.com>
2024-11-09 09:25:05 -0300

    Inclusão da lista de quartos em App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,38 @@
-﻿namespace MauiAppHotel
+﻿using MauiAppHotel.Models;
+
+namespace MauiAppHotel
 {
     public partial class App : Application
     {
+
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto()
+            {
+                descricao = "Suíte Super Luxo",
+                diariaAdulto = 110.0,
+                diariaCrianca = 55.0
+            },
+            new Quarto()
+            {
+                descricao = "Suíte Luxo",
+                diariaAdulto = 80.0,
+                diariaCrianca = 40.0
+            },
+            new Quarto()
+            {
+                descricao = "Suíte Single",
+                diariaAdulto = 50,
+                diariaCrianca = 25
+            },
+            new Quarto()
+            {
+                descricao = "Suíte Crise",
+                diariaAdulto = 25,
+                diariaCrianca = 12.5
+            }
+        };
+
         public App()
         {
             InitializeComponent();
